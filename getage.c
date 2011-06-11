465679369d1a0cbdd45953abd5d00de3c39774ff
Simos Xenitellis <simos@gnome.org>
2011-06-11 19:43:21 +0300

    Get current date from system

--- getage.c
+++ getage.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
 
 int x, y, z, x2, y2, z2, day, month, year;
 
 
 int main(void) {
 
+    time_t now;
+    struct tm* now_struct;
 
-    printf("Πόσο έχει ο μήνας: ");
-    scanf("%d", &x);
+    now = time(NULL);
+    now_struct = gmtime(&now);
 
-    printf("Ποιος μήνας είναι: ");
-    scanf("%d", &y);
-
-    printf("Ποια χρονιά είναι: ");
-    scanf("%d", &z);
-
-
+    /* now_struct holds current date, see http://goo.gl/Mi6UP */
 
     printf("Ποσο ειχε την μερα που γεννηθηκες; ");
     scanf("%d", &x2);
@@ -30,9 +27,9 @@ int main(void) {
 
 
 
-    day = (x-x2);
-    month = (y-y2);
-    year = (z-z2);
+    day = (now_struct->tm_mday - x2);
+    month = (now_struct->tm_mon - y2);
+    year = (now_struct->tm_year + 1900 - z2);
 
     printf("Είσαι %d χρονών, %d μηνών και %d ημερών \n", year, month, day);
 }
