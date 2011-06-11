791c43199d3d8edb5b18db101f4083e476b11fdd
Simos Xenitellis <simos@gnome.org>
2011-06-11 19:31:15 +0300

    Reformatted code using the astyle program

--- getage.c
+++ getage.c
@@ -1,31 +1,31 @@
-    #include <stdio.h>
-    #include <math.h>
-    #include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-    int x, y, z, x2, y2, z2, day, month, year;
+int x, y, z, x2, y2, z2, day, month, year;
 
 
-    int main(void) {
-       
+int main(void) {
+
 
     printf("Πόσο έχει ο μήνας: ");
     scanf("%d", &x);
 
-       printf("Ποιος μήνας είναι: ");
+    printf("Ποιος μήνας είναι: ");
     scanf("%d", &y);
 
-       printf("Ποια χρονιά είναι: ");
+    printf("Ποια χρονιά είναι: ");
     scanf("%d", &z);
 
 
 
-       printf("Ποσο ειχε την μερα που γεννηθηκες; ");
+    printf("Ποσο ειχε την μερα που γεννηθηκες; ");
     scanf("%d", &x2);
 
-       printf("Ποιον μηνα γεννηθηκες: ");
+    printf("Ποιον μηνα γεννηθηκες: ");
     scanf("%d", &y2);
 
-       printf("Ποια χρονιά γεννηθηκες: ");
+    printf("Ποια χρονιά γεννηθηκες: ");
     scanf("%d", &z2);
 
 
@@ -35,4 +35,4 @@
     year = (z-z2);
 
     printf("Είσαι %d χρονών, %d μηνών και %d ημερών \n", year, month, day);
-    }
+}
